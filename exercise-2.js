2458e5b4657a14471452e63dd18d21dc4a4653e1
Haidar Afif Maulana <haidarafifmaulana@gmail.com>
2017-12-18 15:04:57 +0700

    exercise 2 if - else

---
+++ exercise-2.js
@@ -0,0 +1,16 @@
+
+var nama = 'Mikael';
+var peran = 'Penyihir';
+
+if (peran === 'Ksatria') {
+	console.log("Selamat datang di Dunia Proxytia, "+nama);
+	console.log("Halo Ksatria "+nama+", kamu dapat menyerang dengan senjatamu!");
+}else if(peran === 'Tabib') {
+	console.log("Selamat datang di Dunia Proxytia, "+nama);
+	console.log("Halo Tabib "+nama+", kamu akan membantu temanmu yang terluka.");
+} else if(peran === 'Penyihir'){
+	console.log("Selamat datang di Dunia Proxytia, "+nama);
+	console.log("Halo Penyihir "+nama+", ciptakan keajaiban yang membantu kemenanganmu!");
+} else {
+	console.log("Halo "+nama+", Pilih peranmu untuk memulai game!");
+}
