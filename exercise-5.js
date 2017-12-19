1da5f62d368831f77b37142f511930dec33a0208
Haidar Afif Maulana <haidarafifmaulana@gmail.com>
2017-12-19 17:38:00 +0700

    exercise 5 about for

---
+++ exercise-5.js
@@ -0,0 +1,8 @@
+console.log('LOOPING PERTAMA');
+for(var i = 1; i < 21; i++){
+  console.log(i +' - I love coding');
+}
+console.log('LOOPING KEDUA');
+for(var i = 20; i > 0; i--){
+  console.log(i +' - I will become fullstack developer');
+}
