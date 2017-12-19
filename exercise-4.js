c3965ef4d2afee68411651e4998dba9e1a2532dd
Haidar Afif Maulana <haidarafifmaulana@gmail.com>
2017-12-19 17:43:29 +0700

    fix exercise 4 , without if conditional

--- exercise-4.js
+++ exercise-4.js
@@ -3,16 +3,12 @@ var iteration = 1;
 console.log('LOOPING PERTAMA');
 
 while(iteration < 20) {
-  iteration++;
-  if(iteration % 2 == 0 ) {
+  iteration += 2;
   console.log(iteration+' - I love coding');
-  }
 }
 iteration = 20;
 console.log('LOOPING KEDUA');
 while(iteration > 1) {
- if(iteration % 2 == 0 ) {
   console.log(iteration + ' - I will become fullstack developer');
- }
- iteration--;
+ iteration -= 2;
 }
